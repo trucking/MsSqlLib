c9c1a7b02400b599535e56442bb082ff2235f44e
liyanwei <liyanwei1110@163.com>
2019-03-29 13:42:52 +0800

    this commit create a dll.that is a access operate class
    Signed-off-by: liyanwei <liyanwei1110@163.com>

---
+++ AccessLib/AccessLib.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+
+namespace AccessLib
+{
+    public class AccessLib
+    {
+        public AccessLib(string path, string username = "", string password = "")
+        {
+            try
+            {
+                this.path = path;
+                this.Username = username;
+                this.Password = password;
+                
+                this.ConnString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + this.path;
+                this.Conn = new OleDbConnection(this.ConnString);
+                this.Conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("连接失败本地access文件失败:{0}", ex.ToString());
+            }
+        }
+
+        public string ConnString { get; }
+       
+        public string path { get; }
+        public string Username { get; }
+        public string Password { get; }
+        private string sql = "";
+
+
+        public OleDbConnection Conn { get; }
+        public string Version { get; }
+
+        public void CloseConn()
+        {
+            this.Conn.Close();
+        }
+        public string GetVersion()
+        {
+            string version = "";
+            return version;
+        }
+        public string EchoSql()
+        {
+            return this.sql;
+        }
+        /*
+         *    Insert操作，参数分别为表名（string），字段（List<string>），值（List<string>)
+         */
+        public int Insert(string table, List<string> itemList, List<string> valueList)
+        {
+            this.sql = "insert into " + table + " (";
+            foreach (string key in itemList)
+            {
+                this.sql += key + ",";
+            }
+            this.sql = this.sql.TrimEnd(',');
+            this.sql += ") values (";
+            foreach (string value in valueList)
+            {
+                this.sql += "'" + value + "',";
+            }
+            this.sql = this.sql.TrimEnd(',');
+            this.sql += ")";
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            return command.ExecuteNonQuery();
+            //return 1;
+        }
+        /*
+         *  用于一次插入多条数据，Insert的重载
+         */
+        public int Insert(string table, List<string> itemList, List<List<string>> valueListArray)
+        {
+            this.sql = "insert into " + table + " (";
+            foreach (string key in itemList)
+            {
+                this.sql += key + ",";
+            }
+            this.sql = this.sql.TrimEnd(',');
+            this.sql += ") values ";
+            int i = 1;
+            foreach (List<string> value in valueListArray)
+            {
+                if (i == 1)
+                {
+                    this.sql += "(";
+                }
+                else
+                {
+                    this.sql += ",(";
+                }
+                foreach (string v in value)
+                {
+                    this.sql += "'" + v + "',";
+                }
+                this.sql = this.sql.TrimEnd(',');
+                this.sql += ")";
+                i++;
+            }
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            return command.ExecuteNonQuery();
+            //return 1;
+        }
+        /*
+         * delete sql statement is delete from table where ...
+         * where condition is maybe one or more conditions
+         * condition's structure while future 
+         */
+        public int Delete(string table, string condition)
+        {
+            this.sql = "delete from " + table + " where " + condition;
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            return command.ExecuteNonQuery();
+        }
+        /*
+         * Update sql statment is update table set params1 = value1,params2 = value2 ...where condition
+         */
+        public int Update(string table, Dictionary<string, string> keyValues, string condition)
+        {
+            this.sql = "update " + table + " set ";
+            foreach (var d in keyValues)
+            {
+                this.sql += d.Key + " = '" + d.Value + "',";
+            }
+            this.sql = this.sql.TrimEnd(',');
+            this.sql += " where " + condition;
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            return command.ExecuteNonQuery();
+            //return 1;
+        }
+
+        public DataSet Select(List<string> items, string table, string condition)
+        {
+            DataSet ds = new DataSet();
+            this.sql = "select ";
+            foreach (var item in items)
+            {
+                this.sql += item + ",";
+            }
+            this.sql = this.sql.TrimEnd(',');
+            this.sql += " from " + table + " where " + condition;
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            OleDbDataAdapter da = new OleDbDataAdapter(this.sql, this.Conn);
+            da.Fill(ds, table);
+            return ds;
+        }
+
+        public DataSet Select(string table, string condition = null)
+        {
+            DataSet ds = new DataSet();
+            this.sql = "select * from " + table;
+            if (condition != null)
+            {
+                this.sql += " where " + condition;
+            }
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            OleDbDataAdapter da = new OleDbDataAdapter(this.sql, this.Conn);
+            da.Fill(ds, table);
+            return ds;
+        }
+
+        public void ExecuteSql(string sql)
+        {
+            this.sql = sql;
+            OleDbCommand command = new OleDbCommand(this.sql, this.Conn);
+            command.ExecuteNonQuery();
+        }
+    }
+}
+
--- TestMsSqlLib/Program.cs
+++ TestMsSqlLib/Program.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MsSqlLib;
+
 using System.Data;
 using System.Data.SqlClient;
+
 
 namespace TestMsSqlLib
 {
@@ -12,41 +14,49 @@ namespace TestMsSqlLib
     {
         static void Main(string[] args)
         {
-            string server = "hds-039.hichina.com";
-            string database = "hds0390432_db";
-            string username = "hds0390432";
-            string password = "wqd7698888";
+            //string server = "hds-039.hichina.com";
+            string path = "./test.mdb";
+            //string database = "hds0390432_db";
+            //string username = "hds0390432";
+            //string password = "wqd7698888";
             Console.WriteLine(123);
-            //List<string> itemList = new List<string>{ "id","item1","item2"};
-            //List<string> valueList1 = new List<string> { "4","tracy","test1"};
-            //List<string> valueList2 = new List<string> { "5", "judy", "test2" };
-            //List<string> valueList3 = new List<string> { "6", "jorden", "test3" };
-            //List<List<string>> valueListArr = new List<List<string>>();
+            List<string> itemList = new List<string>{ "id","item1","item2"};
+            List<string> valueList1 = new List<string> { "4","tracy","test1"};
+            List<string> valueList2 = new List<string> { "5", "judy", "test2" };
+            List<string> valueList3 = new List<string> { "6", "jorden", "test3" };
+            List<List<string>> valueListArr = new List<List<string>>();
             //valueListArr.Add(valueList1);
             //valueListArr.Add(valueList2);
             //valueListArr.Add(valueList3);
             //SqlConnection conn = new SqlConnection();
             //conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\mssqlsystemresource.mdf;Integrated Security=True;Connect Timeout=3";
             //conn.Open();
-            SqlLib sl = new SqlLib(server,database,username,password);
-            //Console.WriteLine("this is MSSqlLib test fun!");
+            //SqlLib sl = new SqlLib(server,database,username,password);
+            AccessLib.AccessLib accessLib = new AccessLib.AccessLib(path);
+            Console.WriteLine("this is MSSqlLib test fun!");
             //Console.WriteLine(conn.ServerVersion);
-            Console.WriteLine(sl.Version);
+            //Console.WriteLine(sl.Version);
             //sl.Insert("test",itemList, valueListArr);
-            //string sql = "delete from test where id = 4";
+            //accessLib.Insert("test", itemList, valueList2);
+            string sql = "delete from test where id = 4";
             //string sql = "insert into test values ('4','item1','item2')";
-            //Dictionary<string, string> keyValues = new Dictionary<string, string>();
-            //keyValues.Add("item1", "hello");
-            //keyValues.Add("item2", "world");
-            List<string> items = new List<string>{ "id", "item1", "item2" };
-            
+            Dictionary<string, string> keyValues = new Dictionary<string, string>();
+            keyValues.Add("item1", "hello");
+            keyValues.Add("item2", "world");
+            //List<string> items = new List<string>{ "id", "item1", "item2" };
+            //accessLib.Delete("test", "id = 4");
+            //accessLib.Update("test", keyValues, "id = 5");
+            //accessLib.Select("test");
             DataSet ds = new DataSet();
-            ds = sl.Select(items, "test", "id = 5");
-            Console.WriteLine(sl.EchoSql());
+            ds = accessLib.Select(itemList, "test", "id = 5");
+            //Console.WriteLine(sl.EchoSql());
+            Console.WriteLine(accessLib.EchoSql());
             Console.WriteLine("{0},{1},{2}",ds.Tables[0].Rows[0][0].ToString(), ds.Tables[0].Rows[0][1].ToString(), ds.Tables[0].Rows[0][2].ToString());
-            sl.Select("test","id = 5");
-            Console.WriteLine(sl.EchoSql());
-            sl.CloseConn();
+            //sl.Select("test","id = 5");
+            //Console.WriteLine(sl.EchoSql());
+            //sl.CloseConn();
+            accessLib.CloseConn();
+            
         }
     }
 }
